7fef23589562170f8edfaaee61531b183acaa1ed
nunuse <lwandog@gmail.com>
2023-09-11 10:00:20 +0200

    water

--- hungry.py
+++ hungry.py
@@ -5,4 +5,6 @@ if hungry=="yes":
     print("eat pizza")
     print("eat burger")
 else:
-    print("do your homework")
+    thirsty=input("are you thirsty?")
+    if thirsty=="yes":
+        print("drink water")
